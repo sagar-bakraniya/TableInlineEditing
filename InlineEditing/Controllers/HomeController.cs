a39e2e520e193763094ba4cdf36fabdb0316be9b
sagar bakraniya <sagarbakraniya5@gmail.com>
2020-05-17 21:16:54 +0530

    inline editing with jquery

--- InlineEditing/Controllers/HomeController.cs
+++ InlineEditing/Controllers/HomeController.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using InlineEditing.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace InlineEditing.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index () {
-            return View();
+            List<Employee> employees = new List<Employee>() 
+            {
+                new Employee() { ID = 1 , EmpName = "emp 1" , Designation = "designation" , Salary = 59990m } ,
+                new Employee() { ID = 2 , EmpName = "emp 2" , Designation = "designation" , Salary = 30000m } ,
+                new Employee() { ID = 3 , EmpName = "emp 3" , Designation = "designation" , Salary = 59990m } ,
+                new Employee() { ID = 4 , EmpName = "emp 4" , Designation = "designation" , Salary = 6690m } ,
+                new Employee() { ID = 5 , EmpName = "emp 5" , Designation = "designation" , Salary = 79990m } ,
+
+            };
+            GridView grid = new GridView();
+            return View( employees );
         }
 
         public ActionResult About () {
@@ -22,6 +34,41 @@ namespace InlineEditing.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update (Employee employee) {
+            List<Employee> employees = new List<Employee>()
+           {
+                new Employee() { ID = 1 , EmpName = "emp 1" , Designation = "designation" , Salary = 59990m } ,
+                new Employee() { ID = 2 , EmpName = "emp 2" , Designation = "designation" , Salary = 30000m } ,
+                new Employee() { ID = 3 , EmpName = "emp 3" , Designation = "designation" , Salary = 59990m } ,
+                new Employee() { ID = 4 , EmpName = "emp 4" , Designation = "designation" , Salary = 6690m } ,
+                new Employee() { ID = 5 , EmpName = "emp 5" , Designation = "designation" , Salary = 79990m } ,
+
+            };
+
+            // update login
+
+            return View( employees );
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete ( Employee employee ) {
+            List<Employee> employees = new List<Employee>()
+           {
+                new Employee() { ID = 1 , EmpName = "emp 1" , Designation = "designation" , Salary = 59990m } ,
+                new Employee() { ID = 2 , EmpName = "emp 2" , Designation = "designation" , Salary = 30000m } ,
+                new Employee() { ID = 3 , EmpName = "emp 3" , Designation = "designation" , Salary = 59990m } ,
+                new Employee() { ID = 4 , EmpName = "emp 4" , Designation = "designation" , Salary = 6690m } ,
+                new Employee() { ID = 5 , EmpName = "emp 5" , Designation = "designation" , Salary = 79990m } ,
+
+            };
+
+            // update login
+
+            return View( employees );
         }
     }
 }
---
+++ InlineEditing/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿
+namespace InlineEditing.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string EmpName { get; set; }
+        public string Designation { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
